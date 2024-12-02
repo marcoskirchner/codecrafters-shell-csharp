57466e1177a3f0abad3a1df62201fb77cb8b07d4
Marcos Kirchner <marcoskirchner@live.com>
2024-12-02 16:16:16 -0300

    Added exit builtin

--- src/main.cs
+++ src/main.cs
@@ -1,7 +1,45 @@
-string? command;
-do
+using System.Globalization;
+
+
+int exitReturnCode = 0;
+bool shouldExit = false;
+List<string> builtins = ["exit"];
+
+while (!shouldExit)
 {
     Console.Write("$ ");
-    command = Console.ReadLine();
-    Console.WriteLine($"{command}: command not found");
-} while (command != "exit");
+    string command = Console.ReadLine()!;
+
+    string[] parts = command.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    if (!ExecuteBuiltIn(parts))
+    {
+        Console.WriteLine($"{command}: command not found");
+    }
+}
+
+return exitReturnCode;
+
+
+bool ExecuteBuiltIn(string[] parts)
+{
+    if (builtins.Contains(parts[0]))
+    {
+        switch (parts[0])
+        {
+            case "exit":
+                exitReturnCode = int.Parse(parts[1], CultureInfo.InvariantCulture);
+                shouldExit = true;
+                break;
+            default:
+                throw new ShellException($"Unknown builtin {parts[0]}");
+        }
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
+
+
+internal sealed class ShellException(string message) : Exception(message);
