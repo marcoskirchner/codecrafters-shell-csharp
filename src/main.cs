09bf50b7ca7d6c0bfae3c6178d256e31e87c46d2
Marcos Kirchner <marcoskirchner@live.com>
2024-12-02 15:55:35 -0300

    Fixed REPL

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,7 @@
-Console.Write("$ ");
-
 string? command;
 do
 {
+    Console.Write("$ ");
     command = Console.ReadLine();
     Console.WriteLine($"{command}: command not found");
 } while (command != "exit");
